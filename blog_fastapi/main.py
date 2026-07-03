d99b0d5d0b98e0b45daa2e108819c99eb3791fd3
likede10 <2725558687@qq.com>
2026-07-03 17:31:25 +0800

    first commit

---
+++ blog_fastapi/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins, # 允许的源
+    allow_credentials=True, # 允许携带 cookie
+    allow_methods=["*"], # 允许的请求方法
+    allow_headers=["*"], # 允许的请求头
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.get("/test")
+async def test():
+    return {"message": "test"}
